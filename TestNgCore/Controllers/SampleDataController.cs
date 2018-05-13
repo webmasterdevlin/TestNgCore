b08963c3f1573ba1b20f143548aaa49950f19877
webmasterdevlin <webmasterdevlin@gmail.com>
2018-05-13 20:37:47 +0800

    Changes in RD and WS

--- TestNgCore/Controllers/SampleDataController.cs
+++ TestNgCore/Controllers/SampleDataController.cs
@@ -9,7 +9,7 @@ namespace TestNgCore.Controllers
     [Route("api/[controller]")]
     public class SampleDataController : Controller
     {
-        private static string[] Summaries = new[]
+        private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
@@ -29,16 +29,10 @@ namespace TestNgCore.Controllers
         public class WeatherForecast
         {
             public string DateFormatted { get; set; }
-            public int TemperatureC { get; set; }
+            public int TemperatureC { private get; set; }
             public string Summary { get; set; }
 
-            public int TemperatureF
-            {
-                get
-                {
-                    return 32 + (int)(TemperatureC / 0.5556);
-                }
-            }
+            public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
         }
     }
 }
